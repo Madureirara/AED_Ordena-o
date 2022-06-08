d5848a98642c635c540071db9ad25f1b2dbd3e67
Rafael_Madureira <rafinhakd4@gmail.com>
2022-06-08 20:51:15 -0300

    Add files via upload

---
+++ Metodos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Algoritmo_de_ordenacao
+{
+    class Metodos
+    {
+        public void Imprime(int[]vetor) 
+        {
+            Console.Write("[ ");
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write($"{vetor[i]} ,");
+            }
+            Console.WriteLine("]");
+        }
+        public int[] VetorAleatorio(int[]vetor)
+        {           
+            Random r = new Random();
+            for (int i = 0; i < vetor.Length; i++)
+            {                
+                vetor[i] = (int)r.Next(0, 20);
+            }
+            return vetor;
+        }
+        public int[] VetorDecrescente(int n)
+        {
+            int[] vetor=new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                vetor[i] = (vetor.Length - 1) - i;
+            }
+            return vetor;
+        }
+        //public int[] VetorCrescente(int[]vetor)
+        //{
+
+        //}
+        public void Bubble_Sort(int[] vet)
+        {
+            int n=0;
+            n= vet.Length;
+            for (int i = n-2; i < n; i++)
+            {
+                for (int j = n-1; j > n; j++)
+                {
+
+                }
+            }
+        }
+        public void Selection_Sort(int[] vet)
+        {
+            int min_pos;
+            int min_value;
+            int auxiliar;
+            for (int i = 0; i < vet.Length-1; i++)
+            {
+                min_pos = i;
+                min_value = vet[i];
+
+                for (int j = i+1; j <vet.Length; j++)
+                {
+                    if (vet[j] < vet[min_pos])
+                    {
+                        min_pos = j;                     
+                    }                                          
+                }
+                auxiliar = vet[i];
+                vet[i] = vet[min_pos];
+                vet[min_pos] = auxiliar;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Algoritmo_de_ordenacao
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Metodos metodos = new Metodos();
+            int indice = 15;           
+            int[] vetor = new int[indice];
+            int[] vetor2 = new int[] {30,8,88,78,63,67,54,96,49,64};
+
+            //metodos.VetorAleatorio(vetor);
+            //metodos.Imprime(vetor);        
+            //vetor = metodos.VetorDecrescente(indice);
+            //metodos.Imprime(vetor);
+            //metodos.Bubble_Sort(vetor2);
+            metodos.Selection_Sort(vetor);
+        }
+    }
+}
